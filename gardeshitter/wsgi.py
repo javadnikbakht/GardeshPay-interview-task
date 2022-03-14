7bfd2654a7a5f94fe1a68d1e230dd20de4fc1e35
Mohammad Javad Nikbakht <javadnikbakht@mail.com>
2022-03-14 18:37:26 +0330

    refactor: gradeshitter changed to gardeshitter
      - name gradeshitter changed to gardeshitter project and refactor modules in project config

--- gardeshitter/wsgi.py
+++ gardeshitter/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gradeshitter project.
+WSGI config for gardeshitter project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gradeshitter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gardeshitter.settings')
 
 application = get_wsgi_application()
